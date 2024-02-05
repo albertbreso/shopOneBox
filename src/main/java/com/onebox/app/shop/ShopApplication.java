d684ad5c4ad2901302f1b8c0f307f0e5c321b01a
Albert Bresó <albertbreso@gmail.com>
2024-02-05 06:46:07 +0100

    qwerty

--- src/main/java/com/onebox/app/shop/ShopApplication.java
+++ src/main/java/com/onebox/app/shop/ShopApplication.java
@@ -2,7 +2,12 @@ package com.onebox.app.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +17,15 @@ public class ShopApplication {
 		SpringApplication.run(ShopApplication.class, args);
 	}
 
+	@Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SACAViX Spring Boot 3 API -------")
+                        .version("0.11")
+                        .description("Sample app Spring Boot 3 with Swagger")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+    }
 }
